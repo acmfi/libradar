e2135674eb48510f220fc682aa2b2875f74120c8
ElMiamiMan <dieg0farrobaterra@gmail.com>
2020-07-30 05:03:40 +0200

    first progress, managed to open apk files, for now it can list the files inside an apk file

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,108 @@
+pub mod radar {
+	use std::fs;
+	use std::io::BufReader;
+	
+	pub struct APK <'a> {
+		path: &'a str,
+		apk: zip::read::ZipArchive<BufReader<fs::File>>
+	}
+
+	impl <'a> APK <'a> {
+		pub fn new (path: &'a str, reader: BufReader<fs::File>) -> Result<Self,Box<std::error::Error>> {
+			let apk = zip::ZipArchive::new(reader)?;
+			Ok(
+				APK {
+					path: &path,
+					apk: apk
+				}
+			)
+				
+		}
+		
+		pub fn set_path(&mut self, path: &'a str) {
+			self.path = path;
+		}
+
+		pub fn get_path(&self) -> &'a str {
+			self.path
+		}
+
+		pub fn get_apk(&self) -> &zip::ZipArchive<BufReader<fs::File>> {
+			let ret = &self.apk;
+			&ret
+		}
+		
+	}
+		
+	pub fn find_apk(name: &str) -> String {
+		let fname = std::path::Path::new(name);		
+		assert!(fname.exists());
+		name.to_string()
+	}
+
+	pub fn open_apk(name: &str) -> Result<APK,Box<std::error::Error>> {
+		let path = find_apk(&name);
+		let file = fs::File::open(&path)?;
+		let reader = BufReader::new(file);
+		println!("APK file {} opened succesfully",&name);
+		
+		Ok(APK::new(&name,reader).unwrap())
+	}
+
+	pub fn show_apk_contents(apk: &APK) {
+		let list = apk.get_apk().file_names();
+		for name in list {
+			println!("{}",name)
+		}
+	}
+
+	pub fn show_dex_files(apk: &APK) {
+		let list = apk.get_apk().file_names();
+		for name in list {
+			if name.contains("classes"){
+				println!("{}",name)
+			}
+		}
+	}
+}
+
 #[cfg(test)]
 mod tests {
+	use super::radar::*;
+	use super::radar::APK;
+	use std::fs;
+	use std::io::BufReader;
+	
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
     }
+
+	#[test]	
+	fn find_file() {
+		assert_eq!(find_apk("apkchiquita.apk"),"apkchiquita.apk");
+	}
+
+	#[test]
+	fn create_struct() -> Result<(),Box<std::error::Error>> {
+		let file = fs::File::open("./apkchiquita.apk")?;
+		let reader = BufReader::new(file);
+		let _apk = APK::new("./apkchiquita.apk",reader)?;
+		Ok(())
+	}
+	#[test]
+	fn open_file() {
+		
+	}
+
+	#[test]
+	fn show_content() {
+		
+	}
+
+	#[test]
+	fn show_dex() {
+		
+	}
+	
 }
---
+++ src/main.rs
@@ -0,0 +1,28 @@
+mod lib;
+
+fn main () {
+
+	let args: Vec<_> = std::env::args().collect();
+	if args.len() < 2 {
+		println!("An argument is needed in order to work.");
+		println!("Usage: {} <apk filename>",&*args[0]);
+		panic!();
+	}
+
+	use lib::radar::*;	
+
+	println!("Finding APK file...");
+	let apk_path = find_apk(&*args[1]);
+	println!("{}\n",apk_path);
+	
+	println!("Opening APK file...");
+	let apk_file = &open_apk(&*args[1]).unwrap();
+	println!();
+	
+	println!("Listing all contents in APK file...");
+	show_apk_contents(apk_file);
+	println!();
+	
+	println!("Listing .dex files in APK file...");
+	show_dex_files(apk_file);
+}
