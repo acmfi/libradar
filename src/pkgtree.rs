805ad2f9ab921ed06f2d1aeea5505f709e0f3f74
Daniel Domínguez <danieldominguez05@gmail.com>
2020-09-10 12:24:12 +0200

    Fixed the Dex type + some feedback

--- src/pkgtree.rs
+++ src/pkgtree.rs
@@ -1,4 +1,4 @@
-use dex::class;
+use dex::class::Class;
 use dex::Dex;
 use md5::Digest;
 use std::collections::HashMap;
@@ -16,11 +16,38 @@ enum Tree {
     },
 }
 
+// FIXME: This algorithm is wrong. Check the comment in your first commit.
+fn get_invoked_apis(class: Class, api_set: Vec<String>) -> Vec<String> {
+    let ret = Vec::new();
+    for methods in class.methods() {
+        for invoked_method in methods {
+            if api_set.contains(&methods) {
+                ret.append(methods);
+            }
+        }
+    }
+    return ret;
+}
+
+// FIXME: Specify the return type here
+fn calc_hash(lst: Vec<String>) {
+    let ret = Digest::new();
+    for s in lst {
+        ret.update(Bytes::from(s));
+    }
+    return ret.finalize();
+}
+
 impl Tree {
-    pub fn new(dex: Dex<>, api_set: Vec<String>) {
-	let branches = HashMap::new();
-	let name = String::from("L");
-        let tree = Tree::Root {branches, name};
+
+    // FIXME: You have to return the `Tree` thingy
+    // The fix for `Dex` is that a `Dex` is parametrized on some reference to
+    // a buffer of bytes (`u8`s). This syntax I think is the most generic AFAIK to
+    // represent this.
+    pub fn new<R: AsRef<[u8]>>(dex: Dex<R>, api_set: Vec<String>) {
+        let branches = HashMap::new();
+        let name = String::from("L");
+        let tree = Tree::Root { branches, name };
 
         for class in dex.classes() {
             let name = class.name();
@@ -36,23 +63,5 @@ impl Tree {
             };
             tree.branches.insert(name, leaf);
         }
-    }
-    fn get_invoked_apis(class: class, api_set: Vec<String>) -> Vec<String> {
-        let ret = Vec::new();
-        for methods in class.methods() {
-            for invoked_method in methods {
-                if api_set.contains(&methods) {
-                    ret.append(methods);
-                }
-            }
-        }
-        return ret;
-    }
-    fn calc_hash(lst: Vec<String>) {
-        let ret = Digest::new();
-        for s in lst {
-            ret.update(Bytes::from(s));
-        }
-        return ret.finalize();
     }
 }
